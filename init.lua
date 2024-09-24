a254fbbd33db6c9e4ee5f2b14fe83b3fef68d2c2
IniyanKanmani <kiniyan002@gmail.com>
2024-09-24 10:24:59 +0100

    added neo tree

--- init.lua
+++ init.lua
@@ -1050,6 +1050,20 @@ require('lazy').setup({
     end,
   },
 
+  { -- File Tree
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>n', '<cmd>Neotree toggle=true<CR>', {})
+    end,
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
