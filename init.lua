fcdc534666d8cd0dbe9d736151628b56019bfc47
IniyanKanmani <kiniyan002@gmail.com>
2024-09-24 08:28:37 +0100

    added lualine

--- init.lua
+++ init.lua
@@ -1036,6 +1036,20 @@ require('lazy').setup({
     end,
   },
 
+  { -- Status Line
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'palenight',
+          section_separators = { left = '', right = '' },
+          component_separators = '|',
+        },
+      }
+    end,
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
